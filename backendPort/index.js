3f8515451932c707af9efd6eaace75d287a73652
AbhishekKumar <166982217+abhishek13712@users.noreply.github.com>
2025-04-20 15:01:31 +0530

    Update index.js
    To allow COAS so that we can connect frontend and backend

--- backendPort/index.js
+++ backendPort/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
 const Contact=require("./router/Contactus")
+const cors = require('cors');
+const allowedOrigins = [
+	"http://localhost:3000",  // local frontend
+	"https://portfolio-abhishek-kumar-front-git-2a22f9-aks-projects-dc81922d.vercel.app/"  // deployed frontend
+  ];
+  
 
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ["GET", "POST"],
+  credentials: true // if you're using cookies or sessions
+}));
 
 // const database = require("./config/database");
 // const cookieParser = require("cookie-parser");
